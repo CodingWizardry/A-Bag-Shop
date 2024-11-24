2af6e9fb88ccdc61cce3523e9560e2362b4f8f6d
CodingWizardry <muditjain1934@gmail.com>
2024-11-24 02:08:05 -0800

    feat: setup auth

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const ownerModel = require("../models/owner-modal");
 
-router.get("/", function(req, res){
-    res.send("Hello World")
-})
+router.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
-module.exports = router
+router.post("/create", async function (req, res) {
+  let owners = ownerModel.find();   
+  if (owners.length > 0) {
+    return res.send(503).send("You dont have permission to create a new owner");
+  }
+  console.log("owners", owners)
+  
+  const { fullName, email, password } = req.body;
+  let createdOwner = await ownerModel.create({
+      fullName,
+      email,
+      password,
+    });
+    res.status(201).send(createdOwner);
+});
+
+module.exports = router;

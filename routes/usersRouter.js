74dff42e052058cc761f6820d89a72123767f998
mudit-gohash <mudit.jain@gohashinclude.com>
2024-11-24 15:33:38 +0530

    feat: setup

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,29 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const userModal = require("../models/user-modal");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+router.get("/", function (req, res) {
+  res.send("Hello World");
+});
+router.post("/register", async function (req, res) {
+  let { email, fullName, password } = req.body;
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, async function (err, hash) {
+      if (err) return res.send(err.message);
+      else {
+        let user = await userModal.create({
+          email,
+          fullName,
+          password: hash,
+        });
+        let token = jwt.sign({email, id:user._id}, "hy");
+        res.cookie("token" , token)
+        res.send("User created successfully");
+      }
+    });
+  });
 
-router.get("/", function(req, res){
-    res.send("Hello World")
-})
+  res.send(user);
+});
 
-module.exports = router
+module.exports = router;
